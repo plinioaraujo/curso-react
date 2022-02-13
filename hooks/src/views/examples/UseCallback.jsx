4d52b744541f8ddbae2e65282f722def472baefa
Plinio Araujo <plinioglad@gmail.com>
2022-02-13 07:11:35 -0300

    Curso React JS - Hooks - UseMemo e UseCallBack

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useCallback } from "react";
+import { useState } from "react/cjs/react.development";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
+import UseCallbackButtons from "./UseCallbackButtons";
 
 const UseCallback = (props) => {
-    return (
-        <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
 
-export default UseCallback
+ const inc = useCallback(
+    function (delta) {
+        setCount(current => current + delta);
+    },[setCount]
+  )
+
+//   function inc(delta) {
+//       setCount(count + delta);
+//   }
+
+  return (
+    <div className="UseCallback">
+      <PageTitle
+        title="Hook UseCallback"
+        subtitle="Retorna uma função memoizada!"
+      />
+
+      <SectionTitle title="Exercício #1" />
+      <div className="center">
+        <span className="text">{count} </span>
+        <UseCallbackButtons inc={inc}/>
+      </div>
+    </div>
+  );
+};
+
+export default UseCallback;
