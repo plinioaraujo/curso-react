b196a6de516f0d5cb7c228f434463ac4c058ae7f
Plinio Araujo <plinioglad@gmail.com>
2022-02-16 23:09:52 -0300

    Curso React JS - Next.js CSS Global / Modularizado

---
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,12 @@
+import Styles from '../styles/Estiloso.module.css'
+function Estiloso() {
+    return ( 
+        <>
+            <div className={Styles.roxo}>
+                <h1 >estilo usando CSS módulos</h1>
+            </div>
+        </>
+     );
+}
+
+export default Estiloso;
