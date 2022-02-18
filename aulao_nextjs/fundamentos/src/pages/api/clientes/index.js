1c7165c8d64b5d5cd9bb4beafd5d0f722d8f3ceb
Plinio Araujo <plinioglad@gmail.com>
2022-02-18 06:27:42 -0300

    Curso React JS - Usando API #01

---
+++ aulao_nextjs/fundamentos/src/pages/api/clientes/index.js
@@ -0,0 +1,23 @@
+function handler(req,resp) {
+//  resp.status(204).send()
+ 
+
+ if(req.method === 'GET'){
+     handleGet(req,resp)
+ }else{
+     resp.status(405).send()
+ }
+
+ function handleGet(req, resp) {
+    resp.status(200).json({
+        nome: 'João',
+        dados: {
+            endereco: "Rua Teste",
+            bairro: 'Centro'
+        }
+    })
+ }
+
+}
+ 
+export default handler ;
